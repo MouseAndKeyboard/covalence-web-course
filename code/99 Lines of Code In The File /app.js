1e499ba2328e4e7e5a2dea3071920e45574fcfaa
M Nef <howtoeatpineapples@gmail.com>
2019-12-05 18:13:58 +0800

    99 lines of code base

---
+++ code/99 Lines of Code In The File /app.js
@@ -0,0 +1,20 @@
+friends = [
+    "jeff",
+    "sally",
+    "alex",
+    "isaac",
+    "jill"
+]
+
+for (i in friends){
+    friend = friends[i]
+    console.log(friend + ":")
+    for (let i = 99; i > 0; --i) {
+        if (i > 1){
+            console.log(`${i} lines of code in the file, ${i} lines of code; ${friend} strikes one out, clears it all out, ${i - 1} ${i-1 == 1 ? 'line' : 'lines'} of code in the file`)
+        }
+        else{
+            console.log(`1 line of code in the file, 1 line of code; ${friend} strikes one out, clears it all out, no more lines of code in the file`)
+        }
+    }
+}
