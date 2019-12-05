b74d782cbab962c3903c5c5cc0c90c2e0a14624d
M Nef <howtoeatpineapples@gmail.com>
2019-12-05 18:20:16 +0800

    song now displays as html

--- code/99 Lines of Code In The File /app.js
+++ code/99 Lines of Code In The File /app.js
@@ -14,14 +14,27 @@ document.addEventListener('DOMContentLoaded', function(){
 function sing() {
     for (i in friends){
         friend = friends[i]
+
+        let friend_div = document.createElement('div');
+        let friend_header = document.createElement('h3');
+        let friend_header_text = document.createTextNode(friend);
+        friend_header.appendChild(friend_header_text);
+        friend_div.appendChild(friend_header);
+        document.body.appendChild(friend_div);
+
         console.log(friend + ":")
         for (let i = 99; i > 0; --i) {
             if (i > 1){
-                console.log(`${i} lines of code in the file, ${i} lines of code; ${friend} strikes one out, clears it all out, ${i - 1} ${i-1 == 1 ? 'line' : 'lines'} of code in the file`)
+                line = `${i} lines of code in the file, ${i} lines of code; ${friend} strikes one out, clears it all out, ${i - 1} ${i-1 == 1 ? 'line' : 'lines'} of code in the file`
             }
             else{
-                console.log(`1 line of code in the file, 1 line of code; ${friend} strikes one out, clears it all out, no more lines of code in the file`)
+                line = `1 line of code in the file, 1 line of code; ${friend} strikes one out, clears it all out, no more lines of code in the file`
             }
+            line_p = document.createElement('p');
+            line_p_text = document.createTextNode(line);
+            line_p.appendChild(line_p_text);
+
+            friend_div.appendChild(line_p);
         }
     }
 }
