0ca007b3d3d600821fc17e8ef032a2a02041b274
M Nef <howtoeatpineapples@gmail.com>
2019-12-05 13:38:30 +0800

    dom fundamentals

---
+++ code/DOM fundamentals/app.js
@@ -0,0 +1,18 @@
+
+// DOM Workflow
+// create element
+// modify element
+// insert element to document
+
+// create element
+let paragraph = document.createElement("p");
+let text = document.createTextNode("Example paragraph text. Blah blah blah");
+
+// modify element
+paragraph.style.color = "red";
+paragraph.style.textTransform = "uppercase";
+paragraph.className = "some-paragraph"
+paragraph.appendChild(text);
+
+// insert element to document
+document.body.appendChild(paragraph);
