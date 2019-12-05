3048b9d804f5b4cbbd157c127108898d12087de2
M Nef <howtoeatpineapples@gmail.com>
2019-12-05 13:44:12 +0800

    JS now runs when DOM is ready

--- code/DOM fundamentals/app.js
+++ code/DOM fundamentals/app.js
@@ -1,18 +1,23 @@
 
-// DOM Workflow
-// create element
-// modify element
-// insert element to document
+// DOM elements can't be manipulated until the browser has finished 
+// loading the HTML document
+document.addEventListener("DOMContentLoaded", function(){
+    // DOM Workflow
+    // create element
+    // modify element
+    // insert element to document
 
-// create element
-let paragraph = document.createElement("p");
-let text = document.createTextNode("Example paragraph text. Blah blah blah");
+    // create element
+    let paragraph = document.createElement("p");
+    let text = document.createTextNode("Example paragraph text. Blah blah blah");
 
-// modify element
-paragraph.style.color = "red";
-paragraph.style.textTransform = "uppercase";
-paragraph.className = "some-paragraph"
-paragraph.appendChild(text);
+    // modify element
+    paragraph.style.color = "red";
+    paragraph.style.textTransform = "uppercase";
+    paragraph.className = "some-paragraph"
+    paragraph.appendChild(text);
 
-// insert element to document
-document.body.appendChild(paragraph);
+    // insert element to document
+    document.body.appendChild(paragraph);
+});
+
