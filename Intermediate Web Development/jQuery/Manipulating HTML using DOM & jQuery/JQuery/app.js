72941eef7eea2e80865ed9b30fed6fa77ad6b42e
M Nef <howtoeatpineapples@gmail.com>
2019-12-06 11:59:30 +0800

    JQuery js stubs

---
+++ Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/JQuery/app.js
@@ -0,0 +1,32 @@
+function CreateButton() {
+    
+}
+
+function TextBoxButton() {
+
+}
+
+function ChangingDiv() {
+
+}
+
+function ChangingParagraph() {
+
+}
+
+function AddName() {
+
+}
+
+function AddFriend() {
+    
+}
+
+$(document).ready(function(){
+    CreateButton();
+    TextBoxButton();
+    ChangingDiv();
+    ChangingParagraph();
+    AddName();
+    AddFriend();
+});
