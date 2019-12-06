8a3de173ee4a0525bea21464357e1279a67b3fb3
M Nef <howtoeatpineapples@gmail.com>
2019-12-06 19:28:39 +0800

    hoisting function example

--- Advanced Web Development/Deep Dive: JavaScript/Advanced JavaScript Drills/app.js
+++ Advanced Web Development/Deep Dive: JavaScript/Advanced JavaScript Drills/app.js
@@ -1,6 +1,24 @@
 
 // hoisting 
-name = 'Jeff Besos'
+// // name = 'Jeff Besos'
 // var name;
-let name;
-console.log(name);
+// // let name;
+// console.log(name);
+
+
+// setName();
+// function setName(){
+//     var name = 'Jeff Besos'
+//     console.log(name);
+    
+// }
+
+
+console.log('some text');
+let avg = findAvg(2, 2);
+console.log('some text', avg);
+function findAvg(a, b) {
+    console.log('some text');
+    var answer = ( a + b) / 2;
+    return answer;
+}
