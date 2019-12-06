d8c15d12b0497c048a3e183821e7080651fa30be
M Nef <howtoeatpineapples@gmail.com>
2019-12-06 11:04:28 +0800

    DOM alert button done

--- Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/DOM/app.js
+++ Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/DOM/app.js
@@ -1,5 +1,17 @@
 
+function CreateButton(){
+    let buttonHolder = document.getElementById('alertButtonHolder');
+    let button = document.createElement('button');
+    button.className = "btn btn-primary btn-lg";
+    let buttonText = document.createTextNode('Click Me!');
+    button.appendChild(buttonText);
 
+    button.addEventListener('click', function(){
+        alert('Hello there!')
+    })
+
+    buttonHolder.appendChild(button);
+}
 
 document.addEventListener('DOMContentLoaded', function(){
     CreateButton();
