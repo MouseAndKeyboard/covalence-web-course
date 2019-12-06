fd4f8e754ccd279b0fd55741efad0570174cf788
M Nef <howtoeatpineapples@gmail.com>
2019-12-06 12:29:03 +0800

    JQuery Changing paragraph colour done

--- Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/JQuery/app.js
+++ Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/JQuery/app.js
@@ -26,8 +26,15 @@ function ChangingDiv() {
         });
 }
 
-function ChangingParagraph() {
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
+function ChangingParagraph() {
+    $('#changingParagraph').append('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>')
+    .click(function(){
+        $(this).css('color', `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)})`)
+    });
 }
 
 function AddName() {
