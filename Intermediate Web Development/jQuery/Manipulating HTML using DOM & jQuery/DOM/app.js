db65b2fada4bd52a6d27382ab29231681e588388
M Nef <howtoeatpineapples@gmail.com>
2019-12-06 11:11:24 +0800

    DOM random changing text colour done

--- Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/DOM/app.js
+++ Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/DOM/app.js
@@ -39,6 +39,23 @@ function ChangingDiv(){
     divHolder.appendChild(newdiv);
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+function ChangingParagraph(){
+    let paragraphHolder = document.getElementById('changingParagraph');
+    let paragraph = document.createElement('p'); 
+    let paragraphText = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    paragraph.appendChild(paragraphText);
+
+    paragraph.addEventListener('click', function() {
+        paragraph.style.color = `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)})`
+    }); 
+
+    paragraphHolder.appendChild(paragraph);
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     CreateButton();
     TextBoxButton();
