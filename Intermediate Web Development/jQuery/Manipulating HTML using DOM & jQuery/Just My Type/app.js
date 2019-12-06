f359b0159507df70336b9b60cfec17bc7a7aa33a
M Nef <howtoeatpineapples@gmail.com>
2019-12-06 14:33:10 +0800

    currently typed characters are highlighted

--- Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/Just My Type/app.js
+++ Intermediate Web Development/jQuery/Manipulating HTML using DOM & jQuery/Just My Type/app.js
@@ -1,12 +1,31 @@
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+let currentSentence = 0
+let currentCharacter = 0
 
-function setupUppercaseKeyboard(){
+function SetupSentences(){
+    
+    let text = ""
+
+    for (const key in sentences[currentSentence]) {
+        text += `<span>${sentences[currentSentence][key]}</span>`; 
+    }
+
+
+    $('#sentence').html(text)
+}
+
+function HighlightChar(){
+    $(`#sentence span:nth-child(${currentCharacter})`).css('background-color', 'yellow');
+}
+
+function SetupUppercaseKeyboard(){
 
     $('#keyboard-lower-container').css('display', 'block');
     $('#keyboard-upper-container').css('display', 'none');
 
 
     $(document).keyup(function(e){
+        HighlightChar();
         if(e.key == 'Shift'){
             // shift key released
             $('#keyboard-lower-container').css('display', 'block');
@@ -19,6 +38,8 @@ function setupUppercaseKeyboard(){
     });
 
     $(document).keydown(function(e){
+        HighlightChar();
+
         if(e.key == 'Shift'){
             // shift key pressed
             $('#keyboard-lower-container').css('display', 'none');
@@ -29,11 +50,26 @@ function setupUppercaseKeyboard(){
             let value = e.key.charCodeAt(0);
             $(`#${value}`).css('background-color', 'cadetblue');
         }
+
+        let charToMatch = sentences[currentSentence][currentCharacter]
+        if (e.key == charToMatch){
+            currentCharacter = currentCharacter + 1;
+            if (currentCharacter >= sentences[currentSentence].length){
+                currentSentence = currentSentence + 1;
+                SetupSentences();
+                currentCharacter = 0;
+            }
+            
+        }
+
+
+
     });
 
 }
 
 $(document).ready(function(){
-    setupUppercaseKeyboard();
-
+    SetupUppercaseKeyboard();
+    SetupSentences();
+    HighlightChar();
 });
