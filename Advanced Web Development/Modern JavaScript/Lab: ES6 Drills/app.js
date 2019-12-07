416bca57182d8d5976a45588996170ad24cccaeb
M Nef <howtoeatpineapples@gmail.com>
2019-12-07 12:03:43 +0800

    Arrow Functions

--- Advanced Web Development/Modern JavaScript/Lab: ES6 Drills/app.js
+++ Advanced Web Development/Modern JavaScript/Lab: ES6 Drills/app.js
@@ -1,12 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // default parameter values
-    function favMovie(movie = "The Room", name = "world") {
+    let favMovie = (movie = "The Room", name = "world") => {
         // using template literals 
         console.log(`my name is ${name} and my favorite movie is ${movie}`);
     }
 
     favMovie()
     favMovie("The Big Short", "Jeff Besos");
+
+    let getFirstName = (FullName) => {
+        return FullName.split(',')[0];
+    }
+
+    let getFirstNameConcise = FullName => FullName.split(',')[0];
+
+    Fullname = "Jeff,Besos"
+    f1 = getFirstName(Fullname);
+    f2 = getFirstNameConcise(Fullname);
+
+    console.log(f1, f2);
+
+    let statCalc = (x, y) => 
+        ({
+            exponent: x ** y,
+            product: x * y
+        });
+    
+
+    let {exponent, product} = statCalc(5, 2);
+    console.log(`Exponent is: ${exponent}. Product is: ${product}`);
     
 });
