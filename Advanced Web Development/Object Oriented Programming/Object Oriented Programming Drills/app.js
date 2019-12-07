3c27f9c3ca4d4c7f34d4c2e39cfd33ed0cfc718c
M Nef <howtoeatpineapples@gmail.com>
2019-12-07 16:48:08 +0800

    classes / OOP done

--- Advanced Web Development/Object Oriented Programming/Object Oriented Programming Drills/app.js
+++ Advanced Web Development/Object Oriented Programming/Object Oriented Programming Drills/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    p1 = new Person("Jeff Besos", "New York", "50");
 });
 
 class Person {
@@ -12,4 +12,28 @@ class Person {
     aboutMe(){
         console.log(`Hey! My name is ${this.name}, I am ${this.age} years old and I live in ${this.city}`);
     }
+}
+
+class Vehicle {
+    constructor(manufacturer, wheels, type){ 
+        this.manufacturer = manufacturer;
+        this.wheels = wheels;
+        this.type = type
+    }
+
+    aboutVehicle() {
+        console.log(`${type} made by ${manufacturer} with ${wheels} wheels.`);
+    }
+}
+
+class Ute extends Vehicle {
+    constructor(manufacturer, wheels, type, doors){
+        super(manufacturer, wheels, type);
+        this.doors = doors
+        this.truckBed = true;
+    }
+}
+
+class Sedan extends Vehicle {
+    
 }
