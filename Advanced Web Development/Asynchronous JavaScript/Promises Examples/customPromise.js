cc00d9be614865ad90fc4c33909f643fa356e58f
M Nef <howtoeatpineapples@gmail.com>
2019-12-08 12:57:06 +0800

    Static Promise Methods

--- Advanced Web Development/Asynchronous JavaScript/Promises Examples/customPromise.js
+++ Advanced Web Development/Asynchronous JavaScript/Promises Examples/customPromise.js
@@ -1,9 +1,8 @@
-
 function slowEcho(msg, delay) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            
-            if (Math.random() > 0.6){
+
+            if (Math.random() > 0.8) {
                 reject(new Error("Unfortuantly due to a servere weather event, the request could not be processed."));
             }
 
@@ -17,12 +16,27 @@ document.addEventListener("DOMContentLoaded", () => {
     trigger.addEventListener('click', () => {
 
         slowEcho("Hello World!", 3000)
-        .then(message => {
-            console.log(message);
-        })
-        .catch(err => {
-            console.log(err);
-        })
+            .then(message => {
+                console.log(message);
+            })
+            .catch(err => {
+                console.log(err);
+            })
 
     });
+
+    let t2 = document.addEventListener('click', () => {
+
+        Promise.all([
+            slowEcho("Howdy Partner (1s)", 1000),
+            slowEcho("Greetings (2s)", 2000),
+            slowEcho("Nice work! (1.5s)", 1500)
+        ]).then(([p1, p3, p2]) => {
+            console.log(p1, p2, p3);
+        }).catch(err => {
+            console.log(err);
+        });
+
+    });
+
 });
