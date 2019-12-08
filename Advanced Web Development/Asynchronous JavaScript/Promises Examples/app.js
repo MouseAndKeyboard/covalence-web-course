c9dbec1bf7ce0c633101f51236ed472914e138d6
M Nef <howtoeatpineapples@gmail.com>
2019-12-08 12:12:18 +0800

    promise pyramid of doom

--- Advanced Web Development/Asynchronous JavaScript/Promises Examples/app.js
+++ Advanced Web Development/Asynchronous JavaScript/Promises Examples/app.js
@@ -2,9 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
     
 
     getWordOfTheDay()
-    .then((word) => {
+    .then(word => {
         console.log(`The word of the day is: ${word}`);
-    }, (err) => {
+    }, err => {
         console.log(`Uh oh! There was an error: ${err}`);
     });
+
+    // Promise pyramid of doom
+    getWordOfTheDay()
+    .then((word) => {
+        console.log(`The word of the day is: ${word}`);
+        console.log(`Sending to friend`);
+        tellFriend(word)
+        .then(response => {
+            console.log(`Friend responded: ${response}`);
+            
+        }, err => {
+            console.log(err);
+        })        
+    }, err => {
+        console.log(err);
+    })
+
 });
