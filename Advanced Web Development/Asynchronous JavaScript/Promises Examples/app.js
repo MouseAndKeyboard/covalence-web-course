362db2c983a33a987e8d7c5862051833c64d6d7b
M Nef <howtoeatpineapples@gmail.com>
2019-12-08 11:54:13 +0800

    added example promise

--- Advanced Web Development/Asynchronous JavaScript/Promises Examples/app.js
+++ Advanced Web Development/Asynchronous JavaScript/Promises Examples/app.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     
+
+    getWordOfTheDay()
+    .then((word) => {
+        console.log(`The word of the day is: ${word}`);
+    }, (err) => {
+        console.log(`Uh oh! There was an error: ${err}`);
+    });
 });
