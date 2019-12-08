d24616cd312cc869317ae7cc5451760d213cac67
M Nef <howtoeatpineapples@gmail.com>
2019-12-08 14:59:52 +0800

    Promise.then() Chaining

--- Advanced Web Development/Asynchronous JavaScript/Lab: Promising Results/app.js
+++ Advanced Web Development/Asynchronous JavaScript/Lab: Promising Results/app.js
@@ -1,3 +1,42 @@
+// import * as slowmath from "async-math.js"
+
 document.addEventListener("DOMContentLoaded", () => {
-    
+
+    slowMath.add(6, 2)
+        .then(result => {
+            console.log(result);
+            return slowMath.multiply(result, 2);
+        })
+        .then(result => {
+            console.log(result);
+            return slowMath.subtract(result, 3);
+        })
+        .then(result => {
+            console.log(result);
+            return slowMath.add(result, 98);
+        })
+        .then(result => {
+            console.log(result);
+            return slowMath.remainder(result, 2);
+        })
+        .then(result => {
+            console.log(result);
+            return slowMath.multiply(result, 50);
+        })
+        .then(result => {
+            console.log(result);
+            return slowMath.remainder(result, 40);
+        })
+        .then(result => {
+            console.log(result);
+            return slowMath.add(result, 32);
+        })
+        .then(result => {
+            console.log(`The final result is: ${result}`);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+
 });
