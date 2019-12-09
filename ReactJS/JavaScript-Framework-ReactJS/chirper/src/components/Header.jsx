e549f79f0f5ba390f83d15314b673e638e245a9a
M Nef <howtoeatpineapples@gmail.com>
2019-12-09 17:21:44 +0800

    Chirper Added banner react element

---
+++ ReactJS/JavaScript-Framework-ReactJS/chirper/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <div className="jumbotron jumbotron-fluid p-0 m-0">
+            
+            <div className="container">
+                <img src="banner.png" width="25%" height="25%" alt=""/>
+            </div>
+        </div>
+    );
+}
+
+export default Header
