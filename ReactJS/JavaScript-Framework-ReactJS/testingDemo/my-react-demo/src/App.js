218d6b7eb8407b140e9f4cf69d8ac4190c8692df
M Nef <howtoeatpineapples@gmail.com>
2019-12-09 12:43:02 +0800

    converted function-bassed App component to class-based component

--- ReactJS/JavaScript-Framework-ReactJS/testingDemo/my-react-demo/src/App.js
+++ ReactJS/JavaScript-Framework-ReactJS/testingDemo/my-react-demo/src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Comment from './components/Comment'
 
-const App = () => {
+class App extends Component {
+  constructor(props){
+    super(props);
 
-  let element = <h1>Welcome to React</h1>;
-  
-  return (
-    <div>
-      {element}
-      <Comment
-          author={ { name: "Jeff Besos", avatarUrl: "http://www.gstatic.com/tv/thumb/persons/532529/532529_v9_bb.jpg"} }
-          text="How's it going?"
-        /> 
-    </div>
-  );
+    this.state = {
+
+    }
+  }
+
+  render(){
+    let element = <h1>Welcome to React</h1>;
+    return (
+      <div>
+        {element}
+        <Comment
+            author={ { name: "Jeff Besos", avatarUrl: "http://www.gstatic.com/tv/thumb/persons/532529/532529_v9_bb.jpg"} }
+            text="How's it going?"
+          /> 
+      </div>
+    );
+  }
+
 }
 
 export default App;
