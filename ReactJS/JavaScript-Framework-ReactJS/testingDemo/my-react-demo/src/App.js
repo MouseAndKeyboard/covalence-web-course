bb7e55bff09a5cce9a5e0467e11a1f9a98666d92
M Nef <howtoeatpineapples@gmail.com>
2019-12-09 13:40:15 +0800

    handling events and lists

--- ReactJS/JavaScript-Framework-ReactJS/testingDemo/my-react-demo/src/App.js
+++ ReactJS/JavaScript-Framework-ReactJS/testingDemo/my-react-demo/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import Comment from './components/Comment'
+import List from './components/List'
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      name: "Jeff Besos"
+      name: "Jeff Besos",
+      items: [{value: 'a', id: 1}, {value: 'b', id: 2}, {value: 'c', id: 3} ]
     }
   }
 
+  handleInputChange = name => this.setState({ name });
+
   render(){
-    return (
+
+    return (  
       <div>
         <h1>Welcome to React</h1>
         <input 
           type="text" 
           placeholder="What's your name?"
-          onChange={event => this.setState({name: event.target.value}) }
+          onChange={event => this.handleInputChange(event.target.value) }
            />
+        <button>Save Name</button>
+        <List items={this.state.items}/>
         <Comment
             author={ { name: this.state.name, avatarUrl: "http://www.gstatic.com/tv/thumb/persons/532529/532529_v9_bb.jpg"} }
             text="How's it going?"
---
+++ ReactJS/JavaScript-Framework-ReactJS/testingDemo/my-react-demo/src/components/List.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const List = (props) => {
+    let listItems = props.items.map((item) => {
+        return (
+            <li key={item.id}>
+                {item.value}
+            </li>
+        );
+    });
+
+    return (
+        <ul>
+            {listItems}
+        </ul>
+    );
+}
+
+export default List
