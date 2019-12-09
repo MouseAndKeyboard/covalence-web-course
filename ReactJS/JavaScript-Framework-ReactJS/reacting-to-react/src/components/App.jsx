135048d887918e723bbaabb48106894e282dfd8b
M Nef <howtoeatpineapples@gmail.com>
2019-12-09 14:52:09 +0800

    Reacting to React done

--- ReactJS/JavaScript-Framework-ReactJS/reacting-to-react/src/components/App.jsx
+++ ReactJS/JavaScript-Framework-ReactJS/reacting-to-react/src/components/App.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = (props) => {
-    return (
-        <h1>{props.message}</h1>
-    )
+// const App = (props) => {
+//     return (
+//         <h1>{props.message}</h1>
+//     )
+// }
+
+// export default App
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { text: props.message,
+                       inputValue: "",
+                       hasLoaded: false }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            inputValue: event.target.value
+        });
+    }
+
+    handleClick(event) {
+        this.setState((currentState, currentProps) => {
+            return {
+                hasLoaded: !currentState.hasLoaded
+            }
+        });
+    }
+
+    componentDidMount(){
+        this.setState((currentState, currentProps) => {
+            return {
+                hasLoaded: !currentState.hasLoaded
+            }
+        });
+    }
+
+    render() {
+
+        if (this.state.hasLoaded) {
+            return (
+                <React.Fragment>
+                    <h1>{this.props.message}-{this.state.text}</h1>
+                    <input
+                        type="text"
+                        placeholder={this.state.text}
+                        onChange={this.handleChange}
+                    />
+                    <button 
+                        className="clicker"
+                        onClick={this.handleClick}>
+                        Click Me!
+                    </button>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <h1>Loading...</h1>
+                    <button 
+                        className="clicker"
+                        onClick={this.handleClick}>
+                        Click Me!
+                    </button>
+                </React.Fragment>
+            );
+        }
+
+        
+    }
 }
 
 export default App
--- ReactJS/JavaScript-Framework-ReactJS/reacting-to-react/src/index.js
+++ ReactJS/JavaScript-Framework-ReactJS/reacting-to-react/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 import App from './components/App'
 
 ReactDOM.render(
-    <App message="yo mumma" />,
+    <App message="Hello there!" />,
     document.getElementById('root')
 );
 
