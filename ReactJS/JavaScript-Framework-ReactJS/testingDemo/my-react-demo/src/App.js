66033b6e7e4177360329da62479ac11d9f498118
M Nef <howtoeatpineapples@gmail.com>
2019-12-09 09:30:02 +0800

    composing components

--- ReactJS/JavaScript-Framework-ReactJS/testingDemo/my-react-demo/src/App.js
+++ ReactJS/JavaScript-Framework-ReactJS/testingDemo/my-react-demo/src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Welcome = props => {
+  return <h1>Hello, {props.name}</h1>
+}
+
 const App = () => {
   let element = <div>
-    <h1>Hello world!</h1>
-    <input type="text" placeholder="Hi there!" />
+    <Welcome name="World" />
+    <Welcome name="Jeff" />
+    <Welcome name="Howdy" />
   </div>;
   return element;
 }
