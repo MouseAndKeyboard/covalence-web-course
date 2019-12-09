636f879e97098a237a9436a845c58830dfc35427
M Nef <howtoeatpineapples@gmail.com>
2019-12-09 19:18:50 +0800

    REST fetch (get) request added

--- ReactJS/Application-Programming-Interfaces-APIs/react-api-intro/src/App.js
+++ ReactJS/Application-Programming-Interfaces-APIs/react-api-intro/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'isomorphic-fetch';
+import 'es6-promise';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.API_URL = ""
+  }
+
+  componentDidMount() {
+    fetch("http://dummy.restapiexample.com/api/v1/employees")
+    .then(res => res.json())
+    .then(obj => console.log(obj))
+  }
+
+  render(){ 
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
