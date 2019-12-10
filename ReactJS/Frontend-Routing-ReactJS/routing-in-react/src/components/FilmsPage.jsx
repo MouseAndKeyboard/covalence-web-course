94cdd368d51a6826aafe0128a7fbd753ee602125
M Nef <howtoeatpineapples@gmail.com>
2019-12-10 20:00:33 +0800

    FilmsPage now will load film api and show spinner

--- ReactJS/Frontend-Routing-ReactJS/routing-in-react/src/components/FilmsPage.jsx
+++ ReactJS/Frontend-Routing-ReactJS/routing-in-react/src/components/FilmsPage.jsx
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
 
 class FilmsPage extends Component {
     constructor(props) {
         super(props);
         
+
+        this.state = {
+            loading: true
+        }
+    }
+
+    componentDidMount(){        
+        fetch("https://ghibliapi.herokuapp.com/films")
+        .then(response => response.json())
+        .then(obj => {
+            this.setState({loading: false});
+        }); 
     }
 
     render() {
-        return <div>Films</div>
+        if (this.state.loading){
+            return (
+                <div className="container text-center">
+                    <div class="spinner-border m-5" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div></div>
+            );
+        }
+        
     }
 }
 
