50342a9b513906a90c38fb65a1757f144906bb13
M Nef <howtoeatpineapples@gmail.com>
2019-12-10 17:26:50 +0800

    ReactJS Router Walkthrough added a fetch request on page load to Page 1

--- ReactJS/Frontend-Routing-ReactJS/react-router-example/src/components/App.jsx
+++ ReactJS/Frontend-Routing-ReactJS/react-router-example/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 
 import Page1 from './Page1';
 import Page2 from './Page2';
@@ -9,14 +9,12 @@ class App extends Component {
         return (
             <Router>
                 <Fragment>
-                    <Link>
-                        <Link to="/">Go to Page 1</Link>
-                        <Link to="/page2">Go to Page 2</Link>
-                        <Switch>
-                            <Route exact path="/" component={Page1} />
-                            <Route exact path="/page2" component={Page2}/>
-                        </Switch>
-                    </Link>
+                    <Link to="/">Go to Page 1</Link>
+                    <Link to="/page2">Go to Page 2</Link>
+                    <Switch>
+                        <Route exact path="/" component={Page1} />
+                        <Route exact path="/page2" component={Page2} />
+                    </Switch>
                 </Fragment>
             </Router>
         );
--- ReactJS/Frontend-Routing-ReactJS/react-router-example/src/components/Page1.jsx
+++ ReactJS/Frontend-Routing-ReactJS/react-router-example/src/components/Page1.jsx
@@ -1,6 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
 
 class Page1 extends Component {
+    
+    componentDidMount() {
+        fetch("https://ghibliapi.herokuapp.com/films")
+        .then(resp => resp.json())
+        .then(obj => {
+            console.log(obj);
+        });
+    }
+    
     render() {
         return (
             <h1>This is Page 1</h1>
