47b998beb55077bf26e5f8be8e2e58be1cb1e0c7
M Nef <howtoeatpineapples@gmail.com>
2019-12-12 17:35:45 +0800

    Walkthrough: Basic ExpressJS added submit form

--- Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJs-Walkthrough/server/server.js
+++ Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJs-Walkthrough/server/server.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs')
+const bodyParser = require('body-parser') 
+
 let app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-
-app.use((req, res, next) => {
-    // console.log(req.originalUrl);
-    fs.appendFileSync('log.txt', `${req.url}\n`);
-    next();
+app.post('/contact-form', (req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.name);
 });
 
 app.use(express.static(path.join(__dirname, '../public/')))
-
-// app.get('/order/:name', (req, res) => {
-//     let name = req.params.name;
-//     let email = req.query.email;
-//     res.send(`Your name is: ${name} and email is ${email}`);
-// })
 
 app.listen(3000);
