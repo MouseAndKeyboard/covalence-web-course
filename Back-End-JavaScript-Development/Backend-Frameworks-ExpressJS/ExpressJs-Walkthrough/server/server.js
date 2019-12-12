7fc09da2b6c1d88379dc75c6b1c16037ff5b28a0
M Nef <howtoeatpineapples@gmail.com>
2019-12-12 15:17:52 +0800

    Walkthrough: Basic ExpressJS sending files

--- Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJs-Walkthrough/server/server.js
+++ Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJs-Walkthrough/server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
+
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(3000);
