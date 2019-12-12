1ca8354cef9caa32d3e1ff63c3f977ea3ba15be6
M Nef <howtoeatpineapples@gmail.com>
2019-12-12 16:18:23 +0800

    Walkthrough: Basic ExpressJS added logging to file

--- Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJs-Walkthrough/server/server.js
+++ Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJs-Walkthrough/server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs')
 let app = express();
+
+
+
+app.use((req, res, next) => {
+    // console.log(req.originalUrl);
+    fs.appendFileSync('log.txt', `${req.url}\n`);
+    next();
+});
 
 app.use(express.static(path.join(__dirname, '../public/')))
 
-
+// app.get('/order/:name', (req, res) => {
+//     let name = req.params.name;
+//     let email = req.query.email;
+//     res.send(`Your name is: ${name} and email is ${email}`);
+// })
 
 app.listen(3000);
