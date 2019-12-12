1cc9ea37d75a03bb4bab11bfe28537b25eab4325
M Nef <howtoeatpineapples@gmail.com>
2019-12-12 09:28:09 +0800

    Hook It Up homepage displays all users

--- ReactJS/React-Hooks/hook-it-up/src/components/Home.jsx
+++ ReactJS/React-Hooks/hook-it-up/src/components/Home.jsx
@@ -1,14 +1,52 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Hom = (props) => {
-    // const [state, setState] = useState(defaultState);
-    
-    // useEffect(() => {
-    
-    // }, [dependencies]);
-    
-    return <div>1</div>
-    
+const Home = (props) => {
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        let resp = await fetch('https://jsonplaceholder.typicode.com/users');
+        let obj = await resp.json();
+        setUsers(obj);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    let cards = users.map(user => {
+        return (
+            <div key={user.id} className="row row-cols-4 row-cols-md-2">
+                <div className="col mb-4">
+                    <div className="card">
+                        <div className="card-header text-center">{user.name}</div>
+                        <div className="card-body">
+                            <ul className="list-group">
+                                <li className="list-group-item">
+                                    {user.username}
+                                </li>
+                                <li className="list-group-item">
+                                    {user.email}
+                                </li>
+                                <li className="list-group-item">
+                                    {user.phone}
+                                </li>
+                                <li className="list-group-item">
+                                    {user.website}
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <div className="container">
+            { cards }
+        </div>
+    );
+
 }
 
-export default Hom;
+export default Home;
