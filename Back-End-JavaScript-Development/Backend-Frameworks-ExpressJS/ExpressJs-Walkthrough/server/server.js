eb0b8ad648719eeb8e9cd484e8d56af1b6bbb628
M Nef <howtoeatpineapples@gmail.com>
2019-12-12 15:30:25 +0800

    Walkthrough: Basic ExpressJS using express.static to serve a folder

--- Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJs-Walkthrough/server/server.js
+++ Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJs-Walkthrough/server/server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 let app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use(express.static(path.join(__dirname, '../public/')))
+
+
 
 app.listen(3000);
