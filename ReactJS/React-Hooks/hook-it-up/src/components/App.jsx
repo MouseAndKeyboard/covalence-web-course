235acb768742d9fefa1c7ce371eead747fa62b7c
M Nef <howtoeatpineapples@gmail.com>
2019-12-12 08:53:16 +0800

    Hook it up setup done

---
+++ ReactJS/React-Hooks/hook-it-up/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+
+const App = (props) => {
+    // const [state, setState] = useState(defaultState);
+    
+    // useEffect(() => {
+    
+    // }, [dependencies]);
+    
+    return <div></div>
+}
+
+export default App;
