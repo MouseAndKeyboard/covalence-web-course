d6b9c18902f5480f578179cfa026508d6c52e78d
M Nef <howtoeatpineapples@gmail.com>
2019-12-13 14:12:28 +0800

    chirps api done

---
+++ Back-End-JavaScript-Development/Building-an-API-ExpressJS/Lab/server/routes/chirps.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const store = require('../chirpstore');
+
+let chirpsRouter = express.Router();
+
+
+chirpsRouter.get('/chirps/:id?', (req, resp) => {
+    if (req.params.id) {
+        let chirp = store.GetChirp(req.params.id);
+        resp.json(chirp);
+    } else {
+        let chirps = store.GetChirps();
+        resp.json(chirps);
+    }
+});
+
+chirpsRouter.post('/chirps', (req, resp) => {
+    store.CreateChirp({
+        author: req.body.author,
+        message: req.body.message
+    });
+
+    resp.status(200);
+});
+
+chirpsRouter.delete('/chirps/:id', (req, resp) => {
+    store.DeleteChirp(
+        req.params.id
+    );
+
+    resp.status(200);
+});
+
+chirpsRouter.put('/chirps/:id', (req, resp) => {
+    store.UpdateChirp(
+        req.params.id,
+        {
+            author: req.body.author,
+            message: req.body.message
+        }
+    );
+
+    resp.status(200);
+});
+
+module.exports = chirpsRouter;
