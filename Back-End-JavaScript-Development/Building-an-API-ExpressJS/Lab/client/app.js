ad2e0a0093ece9ddda7edde3db3c731501de64ea
M Nef <howtoeatpineapples@gmail.com>
2019-12-13 18:10:15 +0800

    added ability to delete cards and made sure 'nextid' card won't show

--- Back-End-JavaScript-Development/Building-an-API-ExpressJS/Lab/client/app.js
+++ Back-End-JavaScript-Development/Building-an-API-ExpressJS/Lab/client/app.js
@@ -1,4 +1,3 @@
-
 $('#messageSend').click((evt) => {
     evt.preventDefault();
 
@@ -7,34 +6,56 @@ $('#messageSend').click((evt) => {
         message: $('#message')[0].value
     }
 
-    $.ajax('/api/chirps', {contentType: "application/json; charset=utf-8", data: JSON.stringify(toSend), type: 'POST'});
+    $.ajax('/api/chirps', {
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(toSend),
+        type: 'POST'
+    });
+
+    recheck();
 });
 
-$(document).ready(event => {
-    let chirps = $.ajax('/api/chirps', {type: 'GET', dataType: 'json'});
+function recheck() {
+    let chirps = $.ajax('/api/chirps', {
+        type: 'GET',
+        dataType: 'json'
+    });
     chirps.then(x => {
         let postsZone = document.getElementById('posts');
-
+        while (postsZone.firstChild) {
+            postsZone.removeChild(postsZone.firstChild);
+        }
         for (const key in x) {
             if (x.hasOwnProperty(key)) {
-                const element = x[key];
-                let d = document.createElement('div');
-                d.classList.add('card');
-                
-                let h = document.createElement('div');
-                h.classList.add('card-header');
-                h.appendChild(document.createTextNode(element.author));
-        
-                let b = document.createElement('div');
-                b.classList.add('card-body');
-                b.appendChild(document.createTextNode(element.message));
-                
-                d.id = key;
-                d.appendChild(h);
-                d.appendChild(b);
-        
-                postsZone.appendChild(d);    
+                if (key != 'nextid'){
+                    const element = x[key];
+                    let d = document.createElement('div');
+                    d.classList.add('card');
+    
+                    let h = document.createElement('div');
+                    h.classList.add('card-header');
+                    h.appendChild(document.createTextNode(element.author));
+    
+                    let b = document.createElement('div');
+                    b.classList.add('card-body');
+                    b.appendChild(document.createTextNode(element.message));
+    
+                    d.id = key;
+                    d.appendChild(h);
+                    d.appendChild(b);
+    
+                    d.addEventListener('click', (event) => {
+                        $.ajax(`/api/chirps/${d.id}`, {type: 'DELETE'});
+                        d.remove();
+                    });
+    
+                    postsZone.appendChild(d);
+                }               
             }
         }
     });
+}
+
+$(document).ready(event => {
+    recheck();
 });
