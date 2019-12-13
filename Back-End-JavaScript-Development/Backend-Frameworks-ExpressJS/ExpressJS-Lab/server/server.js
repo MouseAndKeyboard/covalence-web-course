ab049c331498929cf4ec9dbd7a37db3cdc24bbdf
M Nef <howtoeatpineapples@gmail.com>
2019-12-13 12:49:02 +0800

    express js testing

--- Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJS-Lab/server/server.js
+++ Back-End-JavaScript-Development/Backend-Frameworks-ExpressJS/ExpressJS-Lab/server/server.js
@@ -1,17 +1,30 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 let app = express();
 
-// app.use(express.static(path.join(__dirname, '../client/')) );
+app.use(bodyParser.urlencoded({
+    extended: false
+ }))
+
+app.post('/formsubmissions', (req, resp) => {
+    const fName = req.body.firstname;
+    const lName = req.body.surname;
+
+    let obj = {firstName: fName, surname: lName}
+    console.log(obj);
+    // let s = JSON.stringify(obj);
+
+    // fs.appendFileSync('./submissions.json', s);
+});
 
 // app.use('/', (req, rsp, next) => {
 //     console.log(req.url);
 //     next();
 // });
 
-app.get('/', (req, rsp) => {
-    rsp.send('Hello from the web server side...')
-})
+app.use(express.static(path.join(__dirname, '../public/')));
 
 app.listen(3000);
