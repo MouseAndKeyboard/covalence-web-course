91962944544eb391711c389d44c7b3725a708668
M Nef <howtoeatpineapples@gmail.com>
2019-12-14 12:39:01 +0800

    trying to update chirpstore helper program to TS

--- TypeScript/TypeScript-and-React/typescript-chirper/src/server/chirpstore.ts
+++ TypeScript/TypeScript-and-React/typescript-chirper/src/server/chirpstore.ts
@@ -1,40 +1,43 @@
 import fs from 'fs';
-let chirps = { nextid: 0 };
+let chirpsStore: { chirps: Array<IChirp>, nextid: number } = { chirps: [], nextid: 0 }
 
 if(fs.existsSync('chirps.json')) {
     
-    chirps = JSON.parse(fs.readFileSync('chirps.json').toString('UTF-8'));
+    chirpsStore = JSON.parse(fs.readFileSync('chirps.json').toString('UTF-8'));
 }
 
 let getChirps = () => {
-    return Object.assign({}, chirps); //create a copy and return it
+    return Object.assign({}, chirpsStore.chirps); //create a copy and return it
 }
 
 let getChirp = (id: number) => {
-    let chirp : IChirp | {nextid: number} = Object.assign({}, chirps[id]); //create a copy and return it
+    let chirp : IChirp = Object.assign({}, chirpsStore.chirps[id]); //create a copy and return it
     return chirp;
 }
 
-let createChirp = (chirp: IChirp) => {
-    chirps[chirps.nextid++] = chirp;
+let createChirp = ({author, message}: {author: string, message: string}) => {
+    let id = chirpsStore.nextid++;
+    chirpsStore.chirps.push({id, author, message});
     writeChirps();
 };
 
 let updateChirp = (id : number, chirp: IChirp) => {
-    chirps[id] = chirp;
+    let index = chirpsStore.chirps.findIndex(elem => elem.id === id);
+    chirpsStore[index] = chirp;
     writeChirps();
 }
 
 let deleteChirp = (id: number) => {
-    delete chirps[id];
+    delete chirpsStore[id];
     writeChirps();
 }
 
 let writeChirps = () => {
-    fs.writeFileSync('chirps.json', JSON.stringify(chirps));
+    fs.writeFileSync('chirps.json', JSON.stringify(chirpsStore));
 };
 
 export interface IChirp {
+    id: number,
     author: string,
     message: string
 }
