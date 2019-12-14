e28e1cf323df531aeeda6668099245087555031a
M Nef <howtoeatpineapples@gmail.com>
2019-12-14 11:58:23 +0800

    converted chirpstore to typescript

---
+++ TypeScript/TypeScript-and-React/typescript-chirper/src/server/chirpstore.ts
@@ -0,0 +1,47 @@
+import fs from 'fs';
+let chirps = { nextid: 0 };
+
+if(fs.existsSync('chirps.json')) {
+    
+    chirps = JSON.parse(fs.readFileSync('chirps.json').toString('UTF-8'));
+}
+
+let getChirps = () => {
+    return Object.assign({}, chirps); //create a copy and return it
+}
+
+let getChirp = (id: number) => {
+    return Object.assign({}, chirps[id]); //create a copy and return it
+}
+
+let createChirp = (chirp: IChirp) => {
+    chirps[chirps.nextid++] = chirp;
+    writeChirps();
+};
+
+let updateChirp = (id : number, chirp: IChirp) => {
+    chirps[id] = chirp;
+    writeChirps();
+}
+
+let deleteChirp = (id: number) => {
+    delete chirps[id];
+    writeChirps();
+}
+
+let writeChirps = () => {
+    fs.writeFileSync('chirps.json', JSON.stringify(chirps));
+};
+
+export interface IChirp {
+    author: string,
+    message: string
+}
+
+module.exports = {
+    CreateChirp: createChirp,
+    DeleteChirp: deleteChirp,
+    GetChirps: getChirps,
+    GetChirp: getChirp,
+    UpdateChirp: updateChirp
+}
