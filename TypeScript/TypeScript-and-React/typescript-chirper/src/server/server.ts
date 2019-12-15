65f48727adcf925378c177113a32480168614cc9
M Nef <howtoeatpineapples@gmail.com>
2019-12-15 15:42:46 +0800

    added logging to see what caused page to not load

--- TypeScript/TypeScript-and-React/typescript-chirper/src/server/server.ts
+++ TypeScript/TypeScript-and-React/typescript-chirper/src/server/server.ts
@@ -1,15 +1,24 @@
 import * as express from 'express';
 import apiRouter from './routes';
+import * as path from 'path'
 
 const app = express();
 
 app.use(express.static('public'));
+
 
 app.use('/', (req, _resp, next) => {
     next();
 });
 
 app.use('/api', apiRouter);
+
+app.use('*', (req, resp, next) => {
+    console.log(path.join(__dirname, '../public/index.html'));
+    console.log(req.url);
+    resp.sendFile(path.join(__dirname, '../public/index.html'));
+   
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
