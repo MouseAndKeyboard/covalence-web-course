bd882a25d6036cf7bf60fc444b2517e64ea2e3e2
M Nef <howtoeatpineapples@gmail.com>
2019-12-15 16:21:03 +0800

    made update message page look nice

--- TypeScript/TypeScript-and-React/typescript-chirper/src/client/components/Edit.tsx
+++ TypeScript/TypeScript-and-React/typescript-chirper/src/client/components/Edit.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './Header';
 import Timeline from './Timeline';
+import { Link } from 'react-router-dom';
 import { RouteComponentProps } from  'react-router-dom';
 
-interface IEditProps extends RouteComponentProps<{id: string}> {
-
+interface IEditProps extends RouteComponentProps {
+    defaultMessage: string
 }
 
 interface IEditState {
@@ -12,15 +13,38 @@ interface IEditState {
 }
 
 
-export default class Edit extends React.Component<IEditProps, IEditState>  {
-    constructor(props : IEditProps){
+class Edit extends React.Component<IEditProps, IEditState>  {
+    constructor(props: IEditProps) {
         super(props);
     }
 
     render() {
         return (
-            <h1>{this.props.match.params.id}</h1>
+            <React.Fragment>
+                <Header />
+                <div className="container d-flex flex-column">
+                    <div className="d-flex flex-column m-4">
+                        <textarea
+
+                            className="form-control lines-2"
+                            placeholder="Message"
+                            value={this.props.defaultMessage}
+                        />
+
+                        <button
+                            className="btn btn-info my-1"
+                        >Update</button>
+                    </div>
+
+                    
+
+                    <button className="btn btn-warning" onClick={this.props.history.goBack}>Go back</button>
+
+                </div>
+            </React.Fragment>
         );
     }
 
 }
+
+export default Edit;
