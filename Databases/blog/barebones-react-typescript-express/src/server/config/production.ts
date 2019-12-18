3bffa6d65ef4d42278ea0d0678f10d72a48d2daa
M Nef <howtoeatpineapples@gmail.com>
2019-12-18 19:08:28 +0800

    added connection environment variables

--- Databases/blog/barebones-react-typescript-express/src/server/config/production.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/config/production.ts
@@ -1,5 +1,9 @@
 export default {
     mysql: {
-        
+        connectionLimit: process.env.SQL_CON_LIM,
+        user: process.env.SQL_USER,
+        password: process.env.SQL_PASS,
+        host: process.env.SQL_HOST,
+        database: process.env.SCHEMA
     }
 }
