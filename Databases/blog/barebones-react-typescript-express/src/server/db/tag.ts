20155147e9c3e8ca2dc0fe487d33dd753cf416c6
M Nef <howtoeatpineapples@gmail.com>
2019-12-18 22:30:43 +0800

    added db api to get all tags for a post

--- Databases/blog/barebones-react-typescript-express/src/server/db/tag.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/db/tag.ts
@@ -1,11 +1,12 @@
-import { Command } from './index'
+import { Command, Query } from './index'
 
 const Insert = async (name: string) => Command("INSERT INTO Tags(name) VALUES (?)", [name]);
 const AddTag = async (postId: number, tagId: number) => Command("INSERT INTO BlogTags(blogid, tagid) VALUES (?, ?)", [postId, tagId]);
 const ClearTags = async (postId: number) => Command("DELETE FROM BlogTags WHERE blogid = ?", [postId]);
-
+const GetTags = async (postId: number) => Query("CALL spBlogTags(?);", [postId]);
 export default {
     Insert,
-    AddTag,
-    ClearTags
+    AddTag, 
+    ClearTags,
+    GetTags
 }
