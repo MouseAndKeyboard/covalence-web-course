20849fd88edba6e2ff9b275c6d4fad22362353de
M Nef <howtoeatpineapples@gmail.com>
2019-12-18 22:30:56 +0800

    delete db api for blogposts

--- Databases/blog/barebones-react-typescript-express/src/server/db/blog.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/db/blog.ts
@@ -4,9 +4,11 @@ const All = async () => Query("SELECT * FROM Blogs");
 const One = async (blogId: number) => Query("SELECT * FROM Blogs WHERE id = ?", [blogId]);
 const Insert = async (title: string, content: string, authorid: number) => Command("INSERT INTO Blogs(title, content, authorid) VALUES (?, ?, ?)", [title, content, authorid]);
 const Update = async (blogId: number, title: string, content: string, authorid: number) => Command("UPDATE Blogs SET title=?, content=?, authorid=? WHERE id = ?", [title, content, authorid, blogId]);
+const Delete = async (blogId: number) => Command("DELETE FROM Blogs WHERE id = ?", [blogId]);
 export default {
     All,
     One,
     Insert,
-    Update
+    Update,
+    Delete
 }
