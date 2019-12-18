5e432cb76cf80dc589b2f7fa9534c41234fad52a
M Nef <howtoeatpineapples@gmail.com>
2019-12-18 21:16:52 +0800

    Insert, Update and GetOne for Blog DB api

--- Databases/blog/barebones-react-typescript-express/src/server/db/blog.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/db/blog.ts
@@ -1,7 +1,12 @@
-import { Query } from './index';
+import { Query, Command } from './index';
 
 const All = async () => Query("SELECT * FROM Blogs");
-
+const One = async (blogId: number) => Query("SELECT * FROM Blogs WHERE id = ?", [blogId]);
+const Insert = async (title: string, content: string, authorid: number) => Command("INSERT INTO Blogs(title, content, authorid) VALUES (?, ?, ?)", [title, content, authorid]);
+const Update = async (blogId: number, title: string, content: string, authorid: number) => Command("UPDATE Blogs SET title=?, content=?, authorid=? WHERE id = ?", [title, content, authorid, blogId]);
 export default {
-    All
+    All,
+    One,
+    Insert,
+    Update
 }
