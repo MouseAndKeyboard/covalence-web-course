35311f05bdaffe3318dd0347ae66315441ed6a87
M Nef <howtoeatpineapples@gmail.com>
2019-12-18 21:19:42 +0800

    added "ClearTags" interface for tags db api

--- Databases/blog/barebones-react-typescript-express/src/server/db/tag.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/db/tag.ts
@@ -2,8 +2,10 @@ import { Command } from './index'
 
 const Insert = async (name: string) => Command("INSERT INTO Tags(name) VALUES (?)", [name]);
 const AddTag = async (postId: number, tagId: number) => Command("INSERT INTO BlogTags(blogid, tagid) VALUES (?, ?)", [postId, tagId]);
+const ClearTags = async (postId: number) => Command("DELETE * FROM BlogTags WHERE blogid = ?", [postId]);
 
 export default {
     Insert,
-    AddTag
+    AddTag,
+    ClearTags
 }
