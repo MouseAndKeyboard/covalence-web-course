e8b0a046084b61d079e8d52cc908bb2ebc0015a7
M Nef <howtoeatpineapples@gmail.com>
2019-12-19 23:59:45 +0800

    made sure the bearer check hook continues by passing (req, res, next)

--- Databases/blog/barebones-react-typescript-express/src/server/routes/api/index.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/routes/api/index.ts
@@ -1,19 +1,19 @@
 import * as express from 'express';
+import * as passport from 'passport';
 
 import blogRouter from './blog';
 import tagRouter from './tags';
-import passport = require('passport');
-
 
 const apiRouter = express.Router();
 
 apiRouter.use((req, res, next) => {
     passport.authenticate('bearer', {session: false}, (err, user, info) => {
+        
         if (user) {
             req.user = user;
         }
         return next();
-    });
+    })(req, res, next);
 });
 
 apiRouter.use('/blog', blogRouter);
