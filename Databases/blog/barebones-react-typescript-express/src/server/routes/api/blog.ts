e984ae07575ff8e145736065de96c27311d842f0
M Nef <howtoeatpineapples@gmail.com>
2019-12-20 00:00:07 +0800

    reorganised

--- Databases/blog/barebones-react-typescript-express/src/server/routes/api/blog.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/routes/api/blog.ts
@@ -4,24 +4,14 @@ import db from '../../db';
 let blogRouter = express.Router();
 
 const isAdmin : express.RequestHandler = (req: any, res, next) => {
-    console.log(req.user);
     
-    if (!req.user || req.user.role !== 'admin'){
+    if (!req.user || req.user.role !== 'guest'){
         return res.sendStatus(401);
     } else {
         return next();
     }
 } 
 
-blogRouter.get('/', async (req, resp) => {
-    try {
-        let allBlogs = await db.blog.All();
-        resp.status(200).json(allBlogs);
-    } catch (error) {
-        console.log(error);
-        resp.sendStatus(500);
-    }
-});
 
 
 blogRouter.get('/:id/tags', async (req, resp) => {
@@ -36,6 +26,7 @@ blogRouter.get('/:id/tags', async (req, resp) => {
 });
 
 blogRouter.get('/:id', isAdmin, async (req, resp) => {
+    
     try {
         let blog = await db.blog.One(Number(req.params.id));
         resp.status(200).json(blog[0]);
@@ -45,29 +36,6 @@ blogRouter.get('/:id', isAdmin, async (req, resp) => {
     }
 });
 
-interface blogPost {
-    title: string,
-    content: string,
-    author: number
-    tags: Array<number>
-}
-
-blogRouter.post('/', async (req, resp) => {
-    try {
-        let post: blogPost = req.body;
-        post.author = 1;
-        let dbData = await db.blog.Insert(post.title, post.content, post.author);
-        let blogId = dbData.insertId;
-        post.tags.forEach(tagId => {            
-            db.tag.AddTag(blogId, tagId);
-        })
-
-        resp.status(200).json(dbData.insertId);
-    } catch (error) {
-        console.log(error);
-        resp.sendStatus(500);
-    }
-});
 
 blogRouter.put('/:id', async (req, resp) => {
     try {
@@ -98,4 +66,39 @@ blogRouter.delete('/:id', async (req, resp) => {
     }
 });
 
+
+blogRouter.get('/', async (req, resp) => {
+    
+    try {
+        let allBlogs = await db.blog.All();
+        resp.status(200).json(allBlogs);
+    } catch (error) {
+        console.log(error);
+        resp.sendStatus(500);
+    }
+});
+
+interface blogPost {
+    title: string,
+    content: string,
+    author: number
+    tags: Array<number>
+}
+
+blogRouter.post('/', async (req, resp) => {
+    try {
+        let post: blogPost = req.body;
+        post.author = 1;
+        let dbData = await db.blog.Insert(post.title, post.content, post.author);
+        let blogId = dbData.insertId;
+        post.tags.forEach(tagId => {            
+            db.tag.AddTag(blogId, tagId);
+        })
+
+        resp.status(200).json(dbData.insertId);
+    } catch (error) {
+        console.log(error);
+        resp.sendStatus(500);
+    }
+});
 export default blogRouter; 
