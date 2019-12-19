678e47445e3ca5c6e8600a0cf90664da8ba51265
M Nef <howtoeatpineapples@gmail.com>
2019-12-19 09:58:22 +0800

    created blog preview card component

--- Databases/blog/barebones-react-typescript-express/src/client/components/BlogPreview.tsx
+++ Databases/blog/barebones-react-typescript-express/src/client/components/BlogPreview.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 interface IBlogPreviewProps {
-
+    title: string,
+    date: string,
+    blogId: number,
+    authorName: string
 }
 
 interface IBlogPreviewState {
@@ -15,10 +18,17 @@ export default class BlogPreview extends React.Component<IBlogPreviewProps, IBlo
         super(params);
     }
 
-    render(){
+    render() {
+
         return (
-            <div className="card">
-                test
+            <div className="card blogPreview shadow-sm">
+                <img src="https://picsum.photos/360/200" className="card-img-top" alt="blog image" />
+                <div className="card-body">
+                    <h4 className="card-title">{this.props.title}</h4>
+                    <h6 className="card-subtitle text-muted">{this.props.date}</h6>
+                    <p className="card-text">{this.props.authorName}</p>
+                    <a href={`/blogs/${this.props.blogId}`} className="btn btn-primary shadow">Read</a>
+                </div>
             </div>
         );
     }
