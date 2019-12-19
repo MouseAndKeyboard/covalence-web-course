3fed4dc89ef2f54faa5f69e37b9ae2fe95409cea
M Nef <howtoeatpineapples@gmail.com>
2019-12-19 23:59:04 +0800

    changed author insert to use individual parameters (for signup)

--- Databases/blog/barebones-react-typescript-express/src/server/db/authors.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/db/authors.ts
@@ -12,7 +12,7 @@ export interface IAuthor {
 const findOneByEmail = async (email: string) => Query("SELECT * FROM Authors WHERE email = ? LIMIT 1;", [email]); 
 const findOneByID = async (id: number) => Query("SELECT * FROM Authors WHERE id = ? LIMIT 1;", [id]); 
 
-const insert = async (user: any) => Command(`INSERT INTO Authors (name, email, password) VALUES ?`, user);
+const insert = async (name: string, email: string, password: string) => Command("INSERT INTO Authors (name, email, password) VALUES (?, ?, ?)", [name, email, password]);
 
 export default {
     findOneByEmail,
