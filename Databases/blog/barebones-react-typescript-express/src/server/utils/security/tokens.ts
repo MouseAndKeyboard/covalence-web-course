1d16f8843456842e1bd60a5d2ecdf5d868250278
M Nef <howtoeatpineapples@gmail.com>
2019-12-20 00:00:49 +0800

    jwt signs whole token, not just the id

--- Databases/blog/barebones-react-typescript-express/src/server/utils/security/tokens.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/utils/security/tokens.ts
@@ -9,14 +9,20 @@ export const CreateToken = async (payload : IPayload) => {
     let tokenid = await db.tokens.Insert(payload.userid);
     payload.accesstokenid = tokenid.insertId;
     payload.unique = crypto.randomBytes(32).toString('hex');
-    let token = jwt.sign(payload.accesstokenid, cfg.auth.secret);
+    let token = jwt.sign(payload, cfg.auth.secret);
     await db.tokens.Update(payload.accesstokenid, token);
     return token;
 } 
 
-export const ValidateToken = async (token : string) => {
+export const ValidateToken = async (token : any) => {    
+    
     let payload: IPayload = <IPayload>jwt.decode(token);
+    
+
     let [accesstokenid] = await db.tokens.findOne(payload.accesstokenid, token);
+
+
+
     if (!accesstokenid) {
         throw new Error('Invalid Token!');
     } else {
