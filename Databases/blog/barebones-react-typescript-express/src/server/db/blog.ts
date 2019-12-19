60f6b8af28952b4ceb81922a1520d18cab6977a5
M Nef <howtoeatpineapples@gmail.com>
2019-12-19 09:58:06 +0800

    updated get all blogs query to be more specific and to return author

--- Databases/blog/barebones-react-typescript-express/src/server/db/blog.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/db/blog.ts
@@ -1,6 +1,6 @@
 import { Query, Command } from './index';
 
-const All = async () => Query("SELECT * FROM Blogs");
+const All = async () => Query("SELECT Blogs.id, Blogs.title, Blogs._created, Authors.name as author FROM Blogs JOIN Authors ON Blogs.authorid = Authors.id;");
 const One = async (blogId: number) => Query("SELECT * FROM Blogs WHERE id = ?", [blogId]);
 const Insert = async (title: string, content: string, authorid: number) => Command("INSERT INTO Blogs(title, content, authorid) VALUES (?, ?, ?)", [title, content, authorid]);
 const Update = async (blogId: number, title: string, content: string, authorid: number) => Command("UPDATE Blogs SET title=?, content=?, authorid=? WHERE id = ?", [title, content, authorid, blogId]);
