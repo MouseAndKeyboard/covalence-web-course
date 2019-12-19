5c5b3e51c9415871c1cc692cafac80732f10d6a4
M Nef <howtoeatpineapples@gmail.com>
2019-12-19 16:13:20 +0800

    fixed token interface

--- Databases/blog/barebones-react-typescript-express/src/server/db/tokens.ts
+++ Databases/blog/barebones-react-typescript-express/src/server/db/tokens.ts
@@ -2,8 +2,8 @@ import { Query, Command } from './index';
 
 const findOne = async (id: number, token: string) => Query("SELECT * FROM Tokens WHERE id = ? AND token = ?;", [id, token]); 
 
-const Insert = async (authorid: number, token: string, expires: string) => Command("INSERT INTO Tokens(authorid, token, expires) VALUES(?,?,?);", [authorid, token, expires]);
-const Update = async (tokenid: number, authorid: number, token: string, expires: string) => Command("UPDATE Tokens SET authorid = ?, token = ?, expires = ? WHERE id = ?;", [authorid, token, expires, tokenid]);
+const Insert = async (authorid: number) => Command("INSERT INTO Tokens(authorid) VALUES(?);", [authorid]);
+const Update = async (tokenid: number, token: string) => Command("UPDATE Tokens SET token = ? WHERE id = ?;", [ token, tokenid]);
 
 export default {
     findOne,
