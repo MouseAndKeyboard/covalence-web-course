06ea86f0999ae017759991e2ab006a7de43f1ada
M Nef <howtoeatpineapples@gmail.com>
2019-12-19 19:25:19 +0800

    added auth endpoints

---
+++ Databases/blog/barebones-react-typescript-express/src/server/routes/auth/login.ts
@@ -0,0 +1,22 @@
+import * as express from 'express';
+import * as passport from 'passport';
+
+import {CreateToken, IPayload} from '../../utils/security/tokens';
+
+const router = express();
+
+router.post('/', passport.authenticate('local'), async (req : any, res, next) => {
+    try {
+        let token = await CreateToken({ userid: req.user.id });
+        res.json({
+            token,
+            role: req.user.role,
+            userid: req.user.id
+        })
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+export default router;
---
+++ Databases/blog/barebones-react-typescript-express/src/server/routes/auth/register.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+
+import db from '../../db/'
+import { HashPassword } from '../../utils/security/passwords';
+import { CreateToken } from '../../utils/security/tokens';
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+    try {
+        let author = req.body;
+        author.password = HashPassword(req.body.password);
+        let result = await db.authors.insert(author);
+        let token = await CreateToken({ userid: result.insertId });
+        res.json({
+            token,
+            role: 'guest',
+            userid: result.insertId
+        });
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+export default router;
