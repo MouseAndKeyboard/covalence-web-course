1e615229c2c91ce42100d285494b05fdc65b62cd
M Nef <howtoeatpineapples@gmail.com>
2019-12-20 13:11:22 +0800

    created login page

--- Databases/blog/barebones-react-typescript-express/src/client/components/Login.tsx
+++ Databases/blog/barebones-react-typescript-express/src/client/components/Login.tsx
@@ -1,25 +1,67 @@
 import React from "react";
+import { json, SetAccessToken } from '../utils/api'
+import { RouteComponentProps } from 'react-router-dom';
 
+
+interface ILoginProps extends RouteComponentProps{
+
+}
 
 interface ILoginState {
+    email: string,
+    password: string
 }
-
-interface ILoginProps {
-
-}
-
 
 export default class Login extends React.Component<ILoginProps, ILoginState>  {
 
     constructor(params: Readonly<ILoginProps>) {
         super(params);
+
+        this.state = {
+            email: "",
+            password: ""
+        }
     }
 
     submit = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
 
-        
+        try {
+            let result = await json('/auth/login', "POST", {
+                email: this.state.email,
+                password: this.state.password
+            });
+
+            if (result){
+                SetAccessToken(result.token, {userid: result.userid, role: result.role});
+                if (result.role === 'admin') {
+                    this.props.history.push('/new');
+                } else {
+                    this.props.history.push('/');
+                }
+            } else {
+                // checking a login status
+            }
+
+        } catch (error) {
+            throw error;
+        }
+
     }
+
+    updateEmail = (event: any) => {
+        let val = event.target.value;
+        this.setState({
+            email: val
+        });
+    }
+
+    updatePassword = (event: any) => {
+        let val = event.target.value;
+        this.setState({
+            password: val
+        });
+    } 
 
     render() {
 
@@ -32,12 +74,14 @@ export default class Login extends React.Component<ILoginProps, ILoginState>  {
                             <div className="form-group">
                                     {/* email */}
                                     <label htmlFor="title">Email</label>
-                                    <input type="email" className="form-control" name="email" id="email" />
+                                    <input type="email" className="form-control" name="email" id="email"
+                                    onChange={this.updateEmail} value={this.state.email} />
                                 </div>
                                 <div className="form-group">
                                     {/* password */}
                                     <label htmlFor="content">Password</label>
-                                    <input type="password" className="form-control" name="password" id="password" />
+                                    <input type="password" className="form-control" name="password" id="password"
+                                    onChange={this.updatePassword} value={this.state.password} />
                                 </div>
                                 <button className="btn btn-primary" onClick={this.submit}>Login</button>
                             </form>
