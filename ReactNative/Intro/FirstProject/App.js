228068b3d24ea5a6fc4aac135a4d00aadd416e50
M Nef <howtoeatpineapples@gmail.com>
2019-12-21 17:29:33 +0800

    playing around in the react-native app

--- ReactNative/Intro/FirstProject/App.js
+++ ReactNative/Intro/FirstProject/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Text style={styles.textStyle}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -12,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    color: 'red',
+    fontSize: 20
+  }
 });
