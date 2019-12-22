dcc22bb4d53c93b6a38e1dfd7d8f9341e0c59361
M Nef <howtoeatpineapples@gmail.com>
2019-12-22 11:57:34 +0800

    initialized app

---
+++ ReactNative/Intro/NativeTS/template.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  placeholderName: 'NativeTS',
+  templateDir: './template',
+}
--- ReactNative/Intro/TSProject/App.tsx
+++ ReactNative/Intro/TSProject/App.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ToastAndroid } from 'react-native';
 
 export default function App() {
+
+  ToastAndroid.show('This is a test toast', ToastAndroid.SHORT);
+  ToastAndroid.showWithGravity(
+    'Gravity toast',
+    ToastAndroid.SHORT,
+    ToastAndroid.CENTER
+  )
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.holder}>
+      <View style={styles.wrapper}>
+        <View style={styles.container}>
+          <Text style={styles.mainText}>TESTING123</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'black',
+    padding: 5
+  },
+  holder: {
+    flex: 1,
+    backgroundColor: 'red',
+    padding: 10
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mainText: {
+    backgroundColor: 'red',
+    color: 'white',
+    fontFamily: 'monospace',
+    fontSize: 40
+  }
 });
